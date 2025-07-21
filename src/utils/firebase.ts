6cca88cde29de4cf396a077e5236ea3ade46ca8c
yash agrawal <agarwalshivam834@gmail.com>
2025-07-22 02:04:45 +0530

    add count metric on why modal open

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,3 +4,8 @@ export const incrementTabClickedCount = async () => {
   const countRef = ref(db, "counts/tab_clicked");
   await update(countRef, { value: increment(1) });
 };
+
+export const incrementWhyModalOpenCount = async () => {
+  const countRef = ref(db, "counts/why_modal_open");
+  await update(countRef, {value: increment(1)});
+}
